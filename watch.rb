4a4773e22ab376eb2e732db6652f03f6064001ca
Nick Quaranto <nick@quaran.to>
2011-03-01 21:19:28 -0500

    watch them tweets

---
+++ watch.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'tweetstream'
+require 'redis'
+
+redis    = Redis.new
+user_ids = File.readlines("user_ids.txt").map(&:to_i)
+
+TweetStream::Daemon.new(ENV['USER'],ENV['PASSWORD']).follow(*user_ids) do |status|
+  handle = status.user.screen_name
+
+  puts "[#{handle}] #{status.text}"
+
+  redis.zincrby "tweets:count", 1, handle
+  status.text.split.each do |word|
+    redis.sadd "tweets:words:#{handle}", word
+  end
+end
